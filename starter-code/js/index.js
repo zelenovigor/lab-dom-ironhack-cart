e3abfc6902c7c3b04899511afd34cdd57cf33b81
Igor Zelenov <igorg.zelenov@gmail.com>
2020-02-08 21:36:21 -0500

    complete

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,78 +1,122 @@
 var $cart = document.querySelector('#cart tbody');
-var $calc = document.getElementById('calc');
-function updateSubtot($product) {
+var $calc = document.getElementById('calc'); // this is the location of the "Calculate prices" button
+
+// let priceProduct = document.querySelector("#cart > tbody > tr > td.pu > span");
+// let quantity = document.querySelector("#cart > tbody > tr > td.qty > label > input[type=number]");
+// let subTotal = document.querySelector("#cart > tbody > tr > td.subtot > span"); // the location of "sub total"
+
+function updateSubtot(product) {
   // Iteration 1.1
-  let priceProduct = document.querySelector("#cart > tbody > tr > td.pu > span").innerHTML;
-  let quantity = document.querySelector("#cart > tbody > tr > td.qty > label > input[type=number]").value;
-  let sub = Number(priceProduct * quantity);
+  console.log(product)
+  let priceProduct = product.querySelector(".pu span").innerText;
+  let quantity = product.querySelector(".qty input").value;
+  console.log("price: " + priceProduct)
+  console.log("quantity: " + quantity )
+  let sub = priceProduct * quantity;
+  console.log("sub: " + sub)
+  // subTotal.textContent = Number(priceProduct.textContent) * Number(quantity.value)
+
   return sub;
+
 }
+
 function calcAll() {
   // Iteration 1.2
-  let product = document.querySelector("#cart > tbody > tr");
-  //all product
-  // ?
-  let sub1 = document.querySelector("#cart > tbody > tr > td.subtot > span");
-  //updeting innerText to let Sub value
-  sub1.innerText = updateSubtot(product);
-  // Iteration 2 // Add another product/ inside calcAll, modify when Calculate prices
-  //<tr class="product"> for ALL! rows of products)
-  let allProduct = document.querySelectorAll('tr.product');
-  allProduct.forEach(row => {
-  //each chid in a row (Product name [0]/ Price unit[1] / Quantity[2] / Sub-total[3] /  Action[4])
-  let price = row.children[1].children[0].innerText;
-  //  Quantity .value(because it has input)
-  let qty = row.querySelector('.qty input').value;
-  //.subtot = .innerText because it's text inside of the table
-  row.querySelector('.subtot span').innerText = price*qty;
-  });// assining innerText to price*qty (new)
- // Iteration 3 Calculate Total price of all 
- //1.sum each product's subtotal to compute the total
- //2.update the HTML with that total value.
- let reducer = (acc,val) => acc + Number(val.innerText)//span = val
- let totalPrice = Array.from(document.querySelectorAll('.subtot span')).reduce(reducer, 0);// need 0 because reduce needs a value
- 
- document.querySelector('body > h2 > span').innerText = totalPrice
+  // let product = document.querySelector("#cart > tbody > tr");
+  // let sub = document.querySelector("#cart > tbody > tr > td.subtot > span"); 
+  // let justText = document.querySelector("#cart > tbody > tr > td.subtot > span").innerHTML
+  // console.log(typeof sub, typeof justText)
+  // sub.innerText = updateSubtot(product);
+  const allProducts = document.querySelectorAll('tr.product');
+  let totalPrice = 0;
+  allProducts.forEach(row => {
+    let price = row.children[1].children[0].innerText;
+    let qty = row.querySelector('.qty input').value;
+    let total = price*qty;
+    // totalPrice += total;
+    row.querySelector('.subtot span').innerText = total;
+  })
+  document.querySelectorAll('.subtot span').forEach(subtot => {
+    totalPrice += Number(subtot.innerText);
+  });
+
+ // let totalPrice = [...document.querySelectorAll('.subtot span')].reduce((acc, val) => acc+Number(val.innerText), 0);
+
+  document.querySelector("body > h2 > span").innerText = totalPrice;
+
 }
 $calc.onclick = calcAll;
-//Iteration 3
-//Associate the "Delete" buttons to click events 
-//For each button, assign a click event/select the wrapper tr /- select the tbody parent/removeChild
-//1. Select all the "Delete" buttons
-let deleteColection =  document.querySelectorAll('.btn-delete');
-for(i =0; i < deleteColection.length; i++){// loop buttons
-  deleteColection[i].onclick = function(e){ // reaction on click
-   this.parentElement.parentElement.remove()//remove parent element
-   console.log(e,this)
+
+/**
+ * Steps for iteration 4
+ * 1. make a collection of all delete buttons
+ * 2. loop through collection and add an onclick event that will call a function removeProduct() 
+ *    (or whatever you want to name the fucntion) that will remove the whole row (i.e. the product)
+ * 3. Create the function to remove the row (i.e. the product) where delect was clicked 
+ */
+
+/**
+ * Resources
+ * onclick - https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers/onclick
+ * getElementsByClassName() - https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementsByClassName 
+ * Intro to events - https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Events 
+ * Event - https://developer.mozilla.org/en-US/docs/Web/API/Event 
+ * Event.currentTarget - https://developer.mozilla.org/en-US/docs/Web/API/Event/currentTarget
+ * Node.parentElement - https://developer.mozilla.org/en-US/docs/Web/API/Node/parentElement 
+ * Comparison of Event Targets - https://developer.mozilla.org/en-US/docs/Web/API/Event/Comparison_of_Event_Targets  
+ * ChildNode.remove() - https://developer.mozilla.org/en-US/docs/Web/API/ChildNode/remove 
+ */
+
+// HTML collection you can only loop using a regualr for loop
+// NodeList (you can create a node list using querySelectorAll) you can use array methods such as forEach
+// NodeListhttps://developer.mozilla.org/en-US/docs/Web/API/NodeList
+
+
+function addDeleteListener(){
+  let deleteBtnCollection = document.getElementsByClassName('btn btn-delete');    // Step 1: make a collection of all delete buttons
+  for(let i = 0; i < deleteBtnCollection.length;i++){    // Step 2: loop through collection to add onclick event that will remove the product
+    deleteBtnCollection[i].onclick = removeProduct; 
   }
 }
-//Iteration 4
-/*
-You should be able to calculate the product's total price.
-That product's price should be included in the total price of the entire Shopping Cart.
-You should be able to delete the product.
-*/
-/*
-let productName = document.querySelector("#cart > tfoot > tr > td:nth-child(1) > input[type=text]")
-let price = document.querySelector("#cart > tfoot > tr > td:nth-child(2) > input[type=number]").value;
-let creatBtn = document.querySelector('#create')
-creatBtn.onclick = createRow;
-function createRow () {
-  let rowHTML  = <tr class="product">
-  <td class="name">
-    <span>IronBubble-head</span>
-  </td>
-  <td class="pu">$<span>25.00</span></td>
-  <td class="qty">
-    <label>
-      <input type="number" value="0" min="0" />
-    </label>
-  </td>
-  <td class="subtot">$<span>0</span></td>
-  <td class="rm">
-    <button class="btn btn-delete">Delete</button>
-  </td>
-</tr>
-// add html to document
-}
-*/
+
+addDeleteListener()
+
+function removeProduct(event) { // Step 3: create the function to remove the row (i.e. the product) where delect was clicked
+  console.log("what", typeof event.currentTarget)
+  console.log("What does event look like: ", event);
+  console.log("What is the current target: ", event.currentTarget);
+  console.log("What is the parent element of the current target: ", event.currentTarget.parentElement);
+  console.log("What is the parent of the parent of the current target: ", event.currentTarget.parentElement.parentElement);
+  event.currentTarget.parentElement.parentElement.remove(); 
+ } 
+
+ 
+ let createBtn = document.querySelector("#create")
+ 
+ createBtn.onclick = createRow
+ 
+ function createRow(){
+   let productName = document.querySelector("#cart > tfoot > tr > td:nth-child(1) > input[type=text]").value 
+   let price = document.querySelector("#cart > tfoot > tr > td:nth-child(2) > input[type=number]").value
+   let rowHTML = `<tr class="product">
+   <td class="name">
+     <span>${productName}</span>
+   </td>
+   <td class="pu">$<span>${price}</span></td>
+   <td class="qty">
+     <label>
+       <input type="number" value="0" min="0" />
+     </label>
+   </td>
+   <td class="subtot">$<span>0</span></td>
+   <td class="rm">
+     <button class="btn btn-delete">Delete</button>
+   </td>
+   </tr>`
+
+   
+   document.querySelector("#cart > tbody").innerHTML += rowHTML
+
+  addDeleteListener()
+
+ }
